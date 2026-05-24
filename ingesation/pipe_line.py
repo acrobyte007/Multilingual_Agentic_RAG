ea41c2a2a56d141a6f0b4241affd9030e0767cb4
acrobyte007 <ajoyprasad2002217@gmail.com>
2026-05-24 21:39:48 +0530

    vector database

---
+++ ingesation/pipe_line.py
@@ -0,0 +1,4 @@
+from ingesation.extraction import extract_text
+from ingesation.cleaning_chunking import process_text
+from ingesation.embedding_model import embedding_process
+from database.vector_database import pinecone_service
