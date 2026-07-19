0c9553448861e1c542a86f189af83869026e7536
acrobyte007 <ajoyprasad2002217@gmail.com>
2026-07-19 17:49:53 +0530

    max token and input query length

--- features/retrieval/schema.py
+++ features/retrieval/schema.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel, Field
 from typing import List, Optional
+from typing_extensions import Annotated
+from pydantic import StringConstraints
 
 class QueryRequest(BaseModel):
     doc_ids: List[str]
-    query: str
+    query: Annotated[str, StringConstraints(strip_whitespace=True, min_length=1, max_length=500)]
     conversation_id: Optional[str] = None
 
 
